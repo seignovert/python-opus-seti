5ae0cb1d916ec87e840fd9523854e1f77d855034
Benoit Seignovert <github@seignovert.fr>
2018-06-01 10:05:54 +0200

    Add DataDict to Mults

--- opus/mults.py
+++ opus/mults.py
@@ -1,27 +1,13 @@
 # -*- coding: utf-8 -*-
 
-class Mults(object):
+from .data import DataDict
+
+class Mults(DataDict):
     def __init__(self, json):
+        DataDict.__init__(self)
         self._field = json['field']
-        self._data = json['mults']
+        self.load(json['mults'])
 
     def __repr__(self):
-        return 'OPUS API Mults for field: {}'.format(self._field)
-
-    def __getitem__(self, attr):
-        return self._data[attr]
-
-    def __len__(self):
-        return len(self._data)
-
-    def __iter__(self):
-        return iter(self._data)
-
-    def keys(self):
-        return self._data.keys()
-
-    def items(self):
-        return self._data.items()
-
-    def values(self):
-        return self._data.values()
+        return 'OPUS API Multiple choice for field: `{}`\n'.format(self._field) + \
+               '\n'.join(' - {} : {}'.format(key, value) for key, value in self.items())
--- tests/test_mults.py
+++ tests/test_mults.py
@@ -11,28 +11,8 @@ def mults():
     json = JSON.loads(open('tests/api/meta/mults/target.json', 'r').read())
     return Mults(json)
 
-def test_mults_meta(mults):
-    assert repr(mults) == 'OPUS API Mults for field: target'
-
-def test_mults_getitem(mults):
-    assert mults['RHEA'] == 20974
-
-def test_mults_iter(mults):
-    for key in mults:
-        assert isinstance(key, six.text_type)
-
-def test_mults_keys(mults):
-    for key in mults.keys():
-        assert isinstance(key, six.text_type)
-
-def test_mults_items(mults):
-    for key, value in mults.items():
-        assert mults[key] == value
-
-def test_mults_values(mults):
-    for value in mults.values():
-        assert isinstance(value, int)
-
-def test_mults_getitem_err(mults):
-    with pytest.raises(KeyError):
-        mults['ABC']
+def test_mults(mults):
+    r = repr(mults)
+    assert 'OPUS API Multiple choice for field: `target`' in r
+    assert 'TITAN' in r
+    assert len(mults) == 63
