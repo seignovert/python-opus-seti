e54bfa9ca35ed661aba3a503bc29e0d9980ca484
Benoit Seignovert <github@seignovert.fr>
2018-09-12 01:37:46 +0200

    Fix Python 2 Cassini CLI url

--- tests/test_cli_cassini.py
+++ tests/test_cli_cassini.py
@@ -26,7 +26,7 @@ def test_cli_vims(api):
     if six.PY3:
         assert responses.calls[0].request.url == 'http://localhost/data.json?target=TITAN&cols=opusid,target,revno,time1,primaryfilespec&instrumentid=Cassini+VIMS&COVIMSswathlength1=2&COVIMSswathwidth1=2&limit=10&page=1'
     else:
-        assert 'surfacegeometrytargetname=TITAN' in responses.calls[0].request.url
+        assert 'target=TITAN' in responses.calls[0].request.url
 
     assert responses.calls[0].response.text == json
     assert len(resp) == 10
